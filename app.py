ec52d8d3fb9679e8aba795bd9db6aaad62a4b1b7
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2025-02-25 16:13:27 +0100

    Refactor app.py to remove Discord presence tracking and clean up related code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import assets.themes.loadThemes as loadThemes
 from audio_separator.separator import Separator
 from assets.i18n.i18n import I18nAuto
 from argparse import ArgumentParser
-from assets.presence.discord_presence import RPCManager, track_presence
+
 
 i18n = I18nAuto()
 
@@ -223,15 +223,6 @@ out_dir = "./outputs"
 models_dir = "./models"
 extensions = (".wav", ".flac", ".mp3", ".ogg", ".opus", ".m4a", ".aiff", ".ac3")
 
-def load_config_presence():
-    with open(config_file, "r", encoding="utf8") as file:
-        config = json.load(file)
-        return config["discord_presence"]
-
-def initialize_presence():
-    if load_config_presence():
-        RPCManager.start_presence()
-
 initialize_presence()
 
 def download_audio(url, output_dir="ytdl"):
@@ -288,7 +279,7 @@ def leaderboard(list_filter):
     except Exception as e:
         return f"Error: {e}"
 
-@track_presence("Performing BS/Mel Roformer Separation")
+
 def roformer_separator(audio, model_key, out_format, segment_size, override_seg_size, overlap, batch_size, norm_thresh, amp_thresh, single_stem, progress=gr.Progress(track_tqdm=True)):
     base_name = os.path.splitext(os.path.basename(audio))[0]
     roformer_model = roformer_models[model_key]
@@ -326,7 +317,7 @@ def roformer_separator(audio, model_key, out_format, segment_size, override_seg_
     except Exception as e:
         raise RuntimeError(f"Roformer separation failed: {e}") from e
 
-@track_presence("Performing MDXC Separationn")
+
 def mdxc_separator(audio, model, out_format, segment_size, override_seg_size, overlap, batch_size, norm_thresh, amp_thresh, single_stem, progress=gr.Progress(track_tqdm=True)):
     base_name = os.path.splitext(os.path.basename(audio))[0]
     try:
@@ -363,7 +354,7 @@ def mdxc_separator(audio, model, out_format, segment_size, override_seg_size, ov
     except Exception as e:
         raise RuntimeError(f"MDX23C separation failed: {e}") from e
 
-@track_presence("Performing MDX-NET Separation")
+
 def mdxnet_separator(audio, model, out_format, hop_length, segment_size, denoise, overlap, batch_size, norm_thresh, amp_thresh, single_stem, progress=gr.Progress(track_tqdm=True)):
     base_name = os.path.splitext(os.path.basename(audio))[0]
     try:
@@ -401,7 +392,7 @@ def mdxnet_separator(audio, model, out_format, hop_length, segment_size, denoise
     except Exception as e:
         raise RuntimeError(f"MDX-NET separation failed: {e}") from e
 
-@track_presence("Performing VR Arch Separation")
+
 def vrarch_separator(audio, model, out_format, window_size, aggression, tta, post_process, post_process_threshold, high_end_process, batch_size, norm_thresh, amp_thresh, single_stem, progress=gr.Progress(track_tqdm=True)):
     base_name = os.path.splitext(os.path.basename(audio))[0]
     try:
@@ -441,7 +432,6 @@ def vrarch_separator(audio, model, out_format, window_size, aggression, tta, pos
     except Exception as e:
         raise RuntimeError(f"VR ARCH separation failed: {e}") from e
 
-@track_presence("Performing Demucs Separation")
 def demucs_separator(audio, model, out_format, shifts, segment_size, segments_enabled, overlap, batch_size, norm_thresh, amp_thresh, progress=gr.Progress(track_tqdm=True)):
     base_name = os.path.splitext(os.path.basename(audio))[0]
     try:
@@ -484,7 +474,7 @@ def update_stems(model):
     else:
         return gr.update(visible=False)
 
-@track_presence("Performing BS/Mel Roformer Batch Separation")
+
 def roformer_batch(path_input, path_output, model_key, out_format, segment_size, override_seg_size, overlap, batch_size, norm_thresh, amp_thresh, single_stem):
     found_files.clear()
     logs.clear()
@@ -535,7 +525,7 @@ def roformer_batch(path_input, path_output, model_key, out_format, segment_size,
             except Exception as e:
                 raise RuntimeError(f"Roformer batch separation failed: {e}") from e
 
-@track_presence("Performing MDXC Batch Separation")
+
 def mdx23c_batch(path_input, path_output, model, out_format, segment_size, override_seg_size, overlap, batch_size, norm_thresh, amp_thresh, single_stem):
     found_files.clear()
     logs.clear()
@@ -585,7 +575,7 @@ def mdx23c_batch(path_input, path_output, model, out_format, segment_size, overr
             except Exception as e:
                 raise RuntimeError(f"Roformer batch separation failed: {e}") from e
 
-@track_presence("Performing MDX-NET Batch Separation")
+
 def mdxnet_batch(path_input, path_output, model, out_format, hop_length, segment_size, denoise, overlap, batch_size, norm_thresh, amp_thresh, single_stem):
     found_files.clear()
     logs.clear()
@@ -636,7 +626,7 @@ def mdxnet_batch(path_input, path_output, model, out_format, hop_length, segment
             except Exception as e:
                 raise RuntimeError(f"Roformer batch separation failed: {e}") from e
 
-@track_presence("Performing VR Arch Batch Separation")
+
 def vrarch_batch(path_input, path_output, model, out_format, window_size, aggression, tta, post_process, post_process_threshold, high_end_process, batch_size, norm_thresh, amp_thresh, single_stem):
     found_files.clear()
     logs.clear()
@@ -689,7 +679,7 @@ def vrarch_batch(path_input, path_output, model, out_format, window_size, aggres
             except Exception as e:
                 raise RuntimeError(f"Roformer batch separation failed: {e}") from e
 
-@track_presence("Performing Demucs Batch Separation")
+
 def demucs_batch(path_input, path_output, model, out_format, shifts, segment_size, segments_enabled, overlap, batch_size, norm_thresh, amp_thresh):
     found_files.clear()
     logs.clear()
